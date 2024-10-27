8ebfb41229dd8630755b9cd4a039569b014ead0e
mazksr <azkasufirman3@gmail.com>
2024-10-28 03:05:49 +0800

    debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,8 @@ import {checkLoggedIn} from "@/app/ServerActions";
 export async function middleware(request: NextRequest) {
     const isLoggedIn = await checkLoggedIn();
     if (!isLoggedIn.logged_in && (request.url.includes("/history"))) {
+        console.log("not logged in on history")
+        console.log(isLoggedIn.logged_in)
         return NextResponse.redirect(new URL("/login", request.url));
     }
     if (isLoggedIn.logged_in && (request.url.includes("/login"))) {
