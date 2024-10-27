de40ce3a3bafbde6a9d8f487fa0442bc2e4d72ab
mazksr <azkasufirman3@gmail.com>
2024-10-28 05:04:42 +0800

    remove debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,14 @@
-import {NextFetchEvent, NextResponse} from 'next/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
+import {checkLoggedIn} from "@/app/ServerActions";
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest, event: NextFetchEvent) {
-    let isLoggedIn;
-    const cookie = request.cookies.get("access_token")
-
-    event.waitUntil(
-        isLoggedIn = fetch(`${process.env.API_URL}/protected`, {
-        cache: "no-store",
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${cookie?.value}`
-        }
-    })
-    )
-
-    const loginStatus = await isLoggedIn;
-
-    if (!loginStatus.ok && (request.url.includes("/history"))) {
-        console.log("not logged in on history")
+export async function middleware(request: NextRequest) {
+    const isLoggedIn = await checkLoggedIn();
+    if (!isLoggedIn.logged_in && (request.url.includes("/history"))) {
         return NextResponse.redirect(new URL("/login", request.url));
     }
-    if (loginStatus.ok && (request.url.includes("/login"))) {
+    if (isLoggedIn.logged_in && (request.url.includes("/login"))) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 }
