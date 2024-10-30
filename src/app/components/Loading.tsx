cc2c012e6aac1ac58ffe085759db6b737a0bbc4d
mazksr <azkasufirman3@gmail.com>
2024-10-30 18:45:16 +0800

    fix loading animation

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -3,7 +3,7 @@ import "./styles.css"
 
 const Loading = () => {
     return (
-        <div className={"w-full flex items-center justify-center mt-20 flex-nowrap"}>
+        <div className={"w-full flex items-center justify-center mt-20"}>
             <div className="flex flex-row gap-2">
                 <div className="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:.7s]"></div>
                 <div className="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:.3s]"></div>
